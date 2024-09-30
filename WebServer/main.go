949f898d67c423b839a1734fbd04dd4795247cd8
El Maestro <kzachoulitis@gmail.com>
2024-09-30 13:27:36 +0300

    server serves html files and downloads json file

--- WebServer/main.go
+++ WebServer/main.go
@@ -1,5 +1,88 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
 
+type Name struct {
+	Name string `json:"name"`
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	http.ServeFile(w, r, "views/index.html")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	http.ServeFile(w, r, "views/hello.html")
+}
+func saveJSON(w http.ResponseWriter, r *http.Request) {
+	// Check if the request method is POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var nameObj Name
+	err := json.NewDecoder(r.Body).Decode(&nameObj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Received name: %s\n", nameObj.Name)
+
+	// Save the nameObj to a JSON file
+	file, err := os.OpenFile("names.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		http.Error(w, "Could not open file", http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		if cerr := file.Close(); cerr != nil {
+			fmt.Println("Error closing file:", cerr)
+		}
+	}()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(nameObj); err != nil {
+		http.Error(w, "Failed to write to file", http.StatusInternalServerError)
+		return
+	}
+
+	jsonData, err := json.Marshal(nameObj)
+	if err != nil {
+		http.Error(w, "Error converting to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := file.WriteString(string(jsonData) + "\n"); err != nil {
+		http.Error(w, "Error writing to file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]string{"status": "success", "name": nameObj.Name}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+	mux.HandleFunc("/save_json", saveJSON)
+	err := http.ListenAndServe(":8000", mux)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
