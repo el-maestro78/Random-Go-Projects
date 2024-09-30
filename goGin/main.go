3f39e97be14a0210118fa9cfc72ba4d562be912a
El Maestro <kzachoulitis@gmail.com>
2024-09-30 14:39:06 +0300

    go gin initiliazed

---
+++ goGin/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
+}
+
+func getPing(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", index)
+	r.GET("/ping", getPing)
+	r.Run(":8000")
+}
