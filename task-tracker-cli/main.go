3ef2a161b968a720d10b583feef8677b268c68d3
El Maestro <kzachoulitis@gmail.com>
2024-10-09 12:36:05 +0300

    all functions added

--- task-tracker-cli/main.go
+++ task-tracker-cli/main.go
@@ -1,9 +1,10 @@
-package task_tracker_cli
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -45,6 +46,19 @@ func loadTasks(filename string) ([]Task, error) {
 	return tasks, err
 }
 
+func currentTaskID(tasks []Task) int {
+	if len(tasks) == 0 {
+		return 0
+	}
+	maxId := 0
+	for i := range tasks {
+		if tasks[i].id > maxId {
+			maxId = tasks[i].id
+		}
+	}
+	return maxId
+}
+
 func printTasks(tasks []Task) {
 	if len(tasks) > 0 {
 		fmt.Println(tasks)
@@ -53,58 +67,178 @@ func printTasks(tasks []Task) {
 	}
 }
 
-func addTask() {
-
+func getStatus(status string) int {
+	switch status {
+	case "todo":
+		return 0
+	case "in-progress":
+		return 1
+	case "done":
+		return 2
+	default:
+		return -1
+	}
 }
 
-func updateTask() {
-
+func printTasksByStatus(tasks []Task, status int) {
+	if len(tasks) == 0 {
+		fmt.Println("There are no tasks stored")
+	} else {
+		var filtered []Task
+		for _, task := range tasks {
+			if task.status == status {
+				filtered = append(filtered, task)
+			}
+		}
+		fmt.Println(filtered)
+	}
 }
 
-func deleteTask() {
+func addTask(tasks []Task, description string, tasksId *int) {
+	task := Task{
+		id:          *tasksId,
+		description: description,
+		status:      0,
+		createdAt:   time.Now().Truncate(24 * time.Hour), //time.Time{},
+		updatedAt:   time.Now().Truncate(24 * time.Hour),
+	}
+	tasks = append(tasks, task)
+	*tasksId++
+}
 
+func updateTask(tasks []Task, taskId int, description string) {
+	var changed = false
+	for i := range tasks {
+		if tasks[i].id == taskId {
+			tasks[i].description = description
+			changed = true
+			break
+		}
+	}
+	if !changed {
+		fmt.Println("No task found")
+	} else {
+		fmt.Println("Task updated")
+	}
+}
+
+func deleteTask(tasks []Task, taskId int) []Task {
+	var changed = false
+	for i := range tasks {
+		if tasks[i].id == taskId {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			changed = true
+			break
+		}
+	}
+	if !changed {
+		fmt.Println("No task found")
+	} else {
+		fmt.Println("Task updated")
+	}
+	return tasks
+}
+
+func markInProgress(tasks []Task, taskId int) {
+	var changed = false
+	for i := range tasks {
+		if tasks[i].id == taskId {
+			tasks[i].status = 1
+			changed = true
+			break
+		}
+	}
+	if !changed {
+		fmt.Println("No task found")
+	} else {
+		fmt.Println("Task updated")
+	}
+}
+
+func markDone(tasks []Task, taskId int) {
+	var changed = false
+	for i := range tasks {
+		if tasks[i].id == taskId {
+			tasks[i].status = 2
+			changed = true
+			break
+		}
+	}
+	if !changed {
+		fmt.Println("No task found")
+	} else {
+		fmt.Println("Task updated")
+	}
 }
 
 func main() {
 	var tasks []Task
+	var tasksId = currentTaskID(tasks)
 	tasks, err := loadTasks("tasks.json")
 	if err != nil {
 		fmt.Println("Empty Tasks")
 	}
-	args := os.Args
-	command := args[1]
 
-	if len(args) < 3 {
-		fmt.Println("Usage: go run main.go [command] [age]")
-		return
-	}
-
-	dateOnly := time.Now().Truncate(24 * time.Hour)
-	fmt.Println("Current Date and Time:", dateOnly)
-	switch command {
-	case add:
-		printTasks(tasks)
-	case update:
-		printTasks(tasks)
-	case Delete:
-		printTasks(tasks)
-	case list:
-		printTasks(tasks)
-	case mark_in_progress:
-		printTasks(tasks)
-	case mark_done:
-		printTasks(tasks)
-	case exit:
-		err := saveTasks(tasks, "tasks.json")
-		if err != nil {
-			fmt.Println("Saved Successfully")
-			os.Exit(0)
-		} else {
-			fmt.Println("Error saving tasks in json", err)
-			os.Exit(1)
+	for { //while True
+		args := os.Args
+		if len(args) < 3 {
+			fmt.Println("Usage: go run main.go [command] [options]")
+			return
 		}
-	default:
-		fmt.Println("Unknown command:", command)
-		fmt.Println("Unknown command:", command)
+		command := args[1]
+
+		switch command {
+		case "add":
+			description := args[2]
+			addTask(tasks, description, &tasksId)
+		case "update":
+			taskID, err := strconv.Atoi(args[2])
+			if err != nil {
+				fmt.Println("Invalid ID type, try again")
+			}
+			description := args[3]
+			updateTask(tasks, taskID, description)
+		case "delete":
+			taskID, err := strconv.Atoi(args[2])
+			if err != nil {
+				fmt.Println("Invalid ID type, try again")
+			}
+			tasks = deleteTask(tasks, taskID)
+		case "list":
+			if len(args) == 1 {
+				printTasks(tasks)
+			} else {
+				status := getStatus(args[2])
+				if status < 0 {
+					fmt.Println("Invalid status type, try again")
+				}
+				printTasksByStatus(tasks, status)
+			}
+		case "mark_in_progress":
+			taskID, err := strconv.Atoi(args[2])
+			if err != nil {
+				fmt.Println("Invalid ID type, try again")
+			}
+			markInProgress(tasks, taskID)
+		case "mark_done":
+			taskID, err := strconv.Atoi(args[2])
+			if err != nil {
+				fmt.Println("Invalid ID type, try again")
+			}
+			markDone(tasks, taskID)
+		case "exit":
+			err := saveTasks(tasks, "tasks.json")
+			if err != nil {
+				fmt.Println("Saved Successfully")
+				os.Exit(0)
+			} else {
+				fmt.Println("Error saving tasks in json", err)
+				os.Exit(1)
+			}
+		default:
+			fmt.Println("Unknown command:", command)
+			fmt.Println("Unknown command:", command)
+		}
 	}
+
 }
