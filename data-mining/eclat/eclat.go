9a2bc4229d00a755931098707623d00a64a47ae0
El Maestro <kzachoulitis@gmail.com>
2024-10-13 21:25:21 +0300

    eclat body update

--- data-mining/eclat/eclat.go
+++ data-mining/eclat/eclat.go
@@ -1,5 +1,25 @@
 package eclat
 
-func eclat(prefix string, items []int, min_support int, freq_itemsets int) {
+import "strconv"
 
+func getTransactionIDs(transactions [][]string) map[string][]int {
+	var transactionIDs = make(map[string][]int)
+	for i, transaction := range transactions {
+		for id := range transaction {
+			id := strconv.Itoa(id)
+			if _, exists := transactionIDs[id]; exists {
+				transactionIDs[id] = append(transactionIDs[id], i)
+			} else {
+				transactionIDs[id] = []int{i}
+			}
+		}
+	}
+	return transactionIDs
+}
+
+func eclat(prefix string, transactions [][]string, min_support int, freq_itemsets int) {
+	transactionIDs := getTransactionIDs(transactions)
+	for id, transaction := range transactionIDs {
+		
+	}
 }
